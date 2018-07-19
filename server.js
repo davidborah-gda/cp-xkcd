124fd290f4dc6b5771f790f9c73d4b40d8bd0a90
David Borah <dborah87@gmail.com>
2018-07-19 19:00:59 -0400

    created our first express server. Set up a route that will rewquest comic json for us using axios and send back to our client browser application

---
+++ server.js
@@ -0,0 +1,30 @@
+
+const express = require('express');
+
+const axios = require('axios');
+
+const server = express();
+
+const port = 7042;
+
+server.get('/', (requestAnimationFrame, response) => {
+  response.send('Hey!!! This is working!');
+});
+
+server.get('/comic', (requestAnimationFrame, response) => {
+  const url = 'https://xkcd.com/614/info.0.json';
+  axios.get(url)
+    .then((comicResponse) => {
+      response.json(comicResponse.data);
+    })
+    .catch((err) => {
+      response.json({
+        msg: 'Somthing went horribly awry',
+        error: err,
+      });
+    });
+});
+
+server.listen(port, () => {
+  console.log(`Now listening on port: ${port}`);
+});
