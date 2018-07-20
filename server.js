1691486b7b896aa52eb0acf7fc344908ee9e1e64
David Borah <dborah87@gmail.com>
2018-07-19 20:28:23 -0400

    updated to be a more dynamic route

--- server.js
+++ server.js
@@ -11,14 +11,15 @@ server.get('/', (requestAnimationFrame, response) => {
   response.send('Hey!!! This is working!');
 });
 
-server.get('/comic', (requestAnimationFrame, response) => {
-  const url = 'https://xkcd.com/614/info.0.json';
+server.get('/comic/:number', (request, response) => {
+  const { number } = request.params;
+  const url = `https://xkcd.com/${number}/info.0.json`;
   axios.get(url)
     .then((comicResponse) => {
       response.json(comicResponse.data);
     })
     .catch((err) => {
-      response.json({
+      response.status(500).json({
         msg: 'Somthing went horribly awry',
         error: err,
       });
